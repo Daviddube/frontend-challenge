b21ee84d510eade8b32ea42970c3f824d77f9fab
David Dube <daviddube91@hotmail.com>
2024-10-06 13:55:38 -0400

    Added signup page form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-import React from "react";
+import React, { createContext } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./navigation/router";
+import { isEmail, useForm } from "@mantine/form";
+
+export const FormContext = createContext(null);
 
 const App = () => {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      firstName: '',
+      email: '',
+      password: ''
+    },
+    //  validate: {
+    //   email: isEmail('Invalid email')
+    // }
+});
   return (
     <MantineProvider>
-      <RouterProvider router={router} />
+      <FormContext.Provider value={{form}}>
+        <RouterProvider router={router} />
+      </FormContext.Provider>
     </MantineProvider>
   );
 };
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,6 +1,7 @@
 import { Button, Center, Paper, PasswordInput, TextInput, Title } from "@mantine/core";
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { FormContext } from "../../App";
 
 const Root = () => {
 let navigate = useNavigate();
@@ -8,28 +9,38 @@ const routeChange = () => {
     navigate("/more-info");
 }
 
+const { form } = useContext(FormContext);
+
     return <>
         <Center>
             <Paper shadow="xs" p="xl">
                 <Title>SIGN UP</Title>
-                <TextInput
-                    label="First Name"
-                    placeholder="FIRST NAME"
-                >
-                </TextInput>
-                <TextInput
-                    label="E-mail"
-                    placeholder="E-MAIL"
-                >
-                </TextInput>
-                <PasswordInput
-                    label="Password"
-                    placeholder="PASSWORD"
-                >
-                </PasswordInput>
-                <Button mt="xl" mb="xl" onClick={routeChange}>
-                    NEXT
-                </Button>
+                <form onSubmit={form.onSubmit(routeChange)}>
+                    <TextInput
+                        label="First Name"
+                        placeholder="FIRST NAME"
+                        key={form.key('firstName')}
+                        {...form.getInputProps('firstName')}
+                    >
+                    </TextInput>
+                    <TextInput
+                        label="E-mail"
+                        placeholder="E-MAIL"
+                        key={form.key('email')}
+                        {...form.getInputProps('email')}
+                    >
+                    </TextInput>
+                    <PasswordInput
+                        label="Password"
+                        placeholder="PASSWORD"
+                        key={form.key('password')}
+                        {...form.getInputProps('password')}
+                    >
+                    </PasswordInput>
+                    <Button mt="xl" mb="xl" type="submit">
+                        NEXT
+                    </Button>
+                </form>
             </Paper>
         </Center>
     </>
