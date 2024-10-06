ca2105b9767a3e5f35fa7e2e493c696a4ce3012e
David Dube <daviddube91@hotmail.com>
2024-10-06 12:30:52 -0400

    Added Confirmation component

---
+++ src/components/Confirmation/Confirmation.jsx
@@ -0,0 +1,32 @@
+import { Button, Center, Paper, Text, Title } from "@mantine/core";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Confirmation = () => {
+    let navigate = useNavigate();
+    const routeChange = (path) => {
+        navigate(path);
+    }
+
+
+    return <>
+        <Center>
+            <Paper shadow="xs" p="xl">
+                <Title>CONFIRMATION</Title>
+                <Text>FIRST NAME: </Text>
+                <Text>E-MAIL: </Text>
+                <Text>PASSWORD: </Text>
+                <Text>FAVORITE COLOR: </Text>
+                <Text>TERMS AND CONDITIONS: </Text>
+                <Button mt="xl" mb="xl" mr="md" onClick={() => routeChange("/more-info")}>
+                    BACK
+                </Button>
+                <Button mt="xl" mb="xl" onClick={() => routeChange("/")}>
+                    SUBMIT
+                </Button>
+            </Paper>
+        </Center>
+    </>
+}
+
+export default Confirmation;
--- src/navigation/router.jsx
+++ src/navigation/router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Root from "../components/Root/Root";
 import AdditionalInfo from "../components/AdditionalInfo/AdditionalInfo";
+import Confirmation from "../components/Confirmation/Confirmation";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -13,6 +14,10 @@ export const router = createBrowserRouter(
             <Route
                 path="more-info"
                 element={<AdditionalInfo />}
+            />
+            <Route
+                path="confirmation"
+                element={<Confirmation />}
             />
         </Route>
     )
