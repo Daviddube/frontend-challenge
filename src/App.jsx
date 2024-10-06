dd193c3922e83177564cfcfa738fad05e8d932ba
David Dube <daviddube91@hotmail.com>
2024-10-06 11:38:01 -0400

    Created signup page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./navigation/router";
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,7 +1,33 @@
+import { Button, Center, Paper, PasswordInput, Space, TextInput, Title } from "@mantine/core";
 import React from "react";
 
 const Root = () => {
-    return <div>hello</div>
+    return <>
+        <Center>
+            <Paper shadow="xs" p="xl">
+                <Title>SIGN UP</Title>
+                <TextInput
+                    label="First Name"
+                    placeholder="FIRST NAME"
+                >
+                </TextInput>
+                <TextInput
+                    label="E-mail"
+                    placeholder="E-MAIL"
+                >
+                </TextInput>
+                <PasswordInput
+                    label="Password"
+                    placeholder="PASSWORD"
+                >
+                </PasswordInput>
+                <Space />
+                <Button mt="xl" mb="xl">
+                    Next
+                </Button>
+            </Paper>
+        </Center>
+    </>
 }
 
 export default Root;
